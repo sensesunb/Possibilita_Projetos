db08a81412084b4b0daa360e6af60039084fd978
Tiago Gallo <tiagodecarvalhopereira@gmail.com>
2017-06-27 18:42:09 -0300

    Atualizaçao dos arquivos

--- software/Mouse/HoughCircles.py
+++ software/Mouse/HoughCircles.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2
 
-import funcoes
-
 #pega um objeto de webcam, no caso pega o primeiro disponivel (indice 0)
 cap = cv2.VideoCapture(0) 
 
@@ -11,21 +9,21 @@ while(True):
     ret, img = cap.read()
 
     #trata a imagem
-    cimg = cv2.medianBlur(img,5)				#filtro Blur
-    cimg = cv2.cvtColor(cimg, cv2.COLOR_BGR2GRAY)	#Colorido para preto e branco
+    cimg = cv2.medianBlur(img,5)                #filtro Blur
+    cimg = cv2.cvtColor(cimg, cv2.COLOR_BGR2GRAY)   #Colorido para preto e branco
 
     #aplica o Hough Circles
     carac = cimg.shape  #pega as caracteristicas da imagem, retorna um vetor [linhas colunas canais] (nesse caso nao retorna os canais pq a imagem ta em preto e brano)
     rows = carac[0]     #armazena a qtd de linhas da img para passar de parametro na hough circles
     
-    circles = cv2.HoughCircles(cimg,			#imagem de entrada
-			       cv2.HOUGH_GRADIENT,	#algoritmo para detectar circulos
-			       2,			#tamanho da imagem dividido por esse numero, no caso 2
-			       rows/4,			#distância mínima em pixels entre os centros dos circulos detectados
-                   param1=200,		#threshold máximo do detector Canny edge detector (chamado por cvHoughCircles)
-                   param2=50,		#threshold mínimo do Canny edge detector (setado em metade do valor anterior)
- 			       minRadius=10,		#raio mínimo do circulo (qualquer circulo com raio menor não vai ser detectado)
-			       maxRadius=40)		#raio máximo do círculo (qualquer círculo com raio maior não vai ser detectado)
+    circles = cv2.HoughCircles(cimg,            #imagem de entrada
+                   cv2.HOUGH_GRADIENT,  #algoritmo para detectar circulos
+                   2,           #tamanho da imagem dividido por esse numero, no caso 2
+                   rows/4,          #distância mínima em pixels entre os centros dos circulos detectados
+                               param1=200,      #threshold máximo do detector Canny edge detector (chamado por cvHoughCircles)
+                               param2=50,       #threshold mínimo do Canny edge detector (setado em metade do valor anterior)
+                   minRadius=10,        #raio mínimo do circulo (qualquer circulo com raio menor não vai ser detectado)
+                   maxRadius=40)        #raio máximo do círculo (qualquer círculo com raio maior não vai ser detectado)
     
     #desenha os circulos na imagem 
     if circles is not None: # Checa se foram encontrados circulos e se encontrados desenha-os na imagem 
@@ -37,9 +35,6 @@ while(True):
     #Mostra o resultado na tela
     cv2.imshow('detected circles',img)
 
-    c = funcoes.soma (2,1)
-    print(c)
-
     #Espera o usuario teclar q para sair do while e encerrar o programa 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -47,7 +42,6 @@ while(True):
 #Ao termino da execucao, libera a camera e mata as janelas 
 cap.release()
 cv2.destroyAllWindows()
-
 
 
 
--- software/Mouse/Mouse.py
+++ software/Mouse/Mouse.py
@@ -1,3 +1,31 @@
 import numpy as np
 import cv2
+import pyautogui, sys
 
+img = cv2.imread ('teste.jpg')
+
+while True:
+	cv2.imshow('imagem',img)
+	x, y = pyautogui.position()
+	#positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+	#print(positionStr, '\n')
+	#print('\b' * len(positionStr), end='', flush=True)
+	
+	a = cv2.waitKey(1)
+	
+	#A seta esquerda foi teclada
+	if a == 81:
+		x -= 1
+		pyautogui.moveTo(x,y,2)
+	#A seta de cima foi teclada
+	if a == 82:
+		y -= 1
+		pyautogui.moveTo(x,y,2)
+	#A seta da direita foi teclada
+	if a == 83:
+		x += 1
+		pyautogui.moveTo(x,y,2)
+	#A seta de baixo foi teclada
+	if a == 84:
+		y += 1
+		pyautogui.moveTo(x,y,2)
